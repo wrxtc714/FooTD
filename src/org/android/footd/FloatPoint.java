01c5e5e48d1a757780ddcb569e38952ba588040d
mass2k <mass2k@gmail.com>
2011-09-04 16:17:19 +0200

    missing class

---
+++ src/org/android/footd/FloatPoint.java
@@ -0,0 +1,14 @@
+package org.android.footd;
+
+public class FloatPoint {
+
+	public float x;
+	public float y;
+	
+	public FloatPoint(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+}
